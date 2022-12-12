f937dad1769d617ef19e0745d350b55d463fc748
Jaeden <jhaueisen97@gmail.com>
2022-12-12 14:34:59 -0800

    for laz to review

---
+++ js/index.js
@@ -0,0 +1,6 @@
+/*const taskManager = {
+    tasks: []
+}
+*/
+const task1 = new taskManager(0);
+console.log('hello')
---
+++ js/taskManager.js
@@ -0,0 +1,22 @@
+class taskManager {
+constructor(currentId = 0) {
+    this.tasks = [];
+    this.currentId = currentId;
+}
+// Create the addTask method
+addTask(name, description, assignedTo, dueDate) {
+    const task = {
+        // Increment the currentId property
+        id: this.currentId++,
+        name: name,
+        description: description,
+        assignedTo: assignedTo,
+        dueDate: dueDate,
+        status: 'TODO'
+    };
+    // Push the task to the tasks property
+    this.tasks.push(task);
+}
+}
+
+
